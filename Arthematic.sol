4578df4f5abe0e360cff11a8eaf52a39ff147e1c
animal-23 <148389990+animal-23@users.noreply.github.com>
2024-05-28 07:15:35 +0530

    Create Arthematic.sol

---
+++ Arthematic.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract Arthematic {
+    uint16 public a=81;
+    uint16 public b=9;
+    uint public sum=a+b;
+    uint public diff=a-b;
+    uint public prod=a*b;
+    uint public div=a/b;
+    uint public mod=a%b;
+    uint public inc=++a;
+    uint public dec=--b;
+}
